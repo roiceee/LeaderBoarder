b6b8bfffba47588984f5f17eeb78f9accdb3016d
John Roice Aldeza <jroicealdeza@gmail.com>
2024-12-09 00:17:12 +0800

    Update FastifyRequest interface to use CognitoAccessTokenPayload for user property

--- backend/src/@types/request.d.ts
+++ backend/src/@types/request.d.ts
@@ -4,6 +4,6 @@ import { CognitoIdOrAccessTokenPayload } from "aws-jwt-verify/jwt-model";
 // Use module augmentation to extend the FastifyRequest interface
 declare module "fastify" {
   interface FastifyRequest {
-    user?: CognitoIdOrAccessTokenPayload; // Use optional if the property is not guaranteed to exist
+    user?: CognitoAccessTokenPayload; // Use optional if the property is not guaranteed to exist
   }
 }
